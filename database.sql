87f0b4091eca8372a9dd3fe515e93bcfda10b1f6
Ruan <silvacipriano2005@gmail.com>
2024-08-02 16:43:59 -0300

    Ruan

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE yourdbname;
+CREATE DATABASE CRUD;
 
-USE yourdbname;
+USE CRUD;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,3 +8,4 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL
 );
+
